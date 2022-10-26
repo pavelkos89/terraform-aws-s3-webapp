f080d1025010ccb4f2f2b16c36813283c8a2fc7e
Pavlo Kosiakin <Pavlo.Kosiakin@infopulse.com>
2022-10-26 17:55:35 +0300

    test

--- main.tf
+++ main.tf
@@ -1,53 +1,20 @@
-resource "aws_s3_bucket" "bucket" {
-  bucket_prefix = "${var.prefix}-${var.name}"
-
-  force_destroy = true
-}
-
-resource "aws_s3_bucket_website_configuration" "bucket" {
-  bucket = aws_s3_bucket.bucket.id
-
-  index_document {
-    suffix = "index.html"
-  }
-
-  error_document {
-    key = "error.html"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0.0"
+    }
   }
 }
 
-resource "aws_s3_bucket_acl" "bucket" {
-  bucket = aws_s3_bucket.bucket.id
-
-  acl = "public-read"
+provider "aws" {
+  region = var.region
 }
 
-resource "aws_s3_bucket_policy" "policy" {
-  bucket = aws_s3_bucket.bucket.id
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "PublicReadGetObject",
-            "Effect": "Allow",
-            "Principal": "*",
-            "Action": [
-                "s3:GetObject"
-            ],
-            "Resource": [
-                "arn:aws:s3:::${aws_s3_bucket.bucket.id}/*"
-            ]
-        }
-    ]
-}
-EOF
-}
-
-resource "aws_s3_object" "webapp" {
-  acl          = "public-read"
-  key          = "index.html"
-  bucket       = aws_s3_bucket.bucket.id
-  content      = file("${path.module}/assets/index.html")
-  content_type = "text/html"
+module "s3-webapp" {
+  source  = "app.terraform.io/kosiakin/terraform/terraform-aws-s3-webapp/aws"
+  name   = var.name
+  region = var.region
+  prefix = var.prefix
+  version = "1.0.0"
 }
